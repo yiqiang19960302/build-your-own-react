2f866e81cb303095e9662a66124b2eb7be66a788
Yi Qiang <yiqiang1996@gmail.com>
2025-05-27 22:35:21 +0800

    inited the project

---
+++ build/precache-manifest.de9d7756a2836177117d2eb01e760e9a.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e91c696726ed75b97c7c5de958f874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db9b840a7610de07c97",
+    "url": "/static/js/main.02632b4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
